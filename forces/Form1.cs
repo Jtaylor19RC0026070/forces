43620c136d58ac7a74355915ea11de73d3911f61
Jack Taylor <RC0026070@student.riversidecollege.ac.uk>
2018-12-04 10:50:50 +0000

    sin cos tan and inverse

--- forces/Form1.cs
+++ forces/Form1.cs
@@ -16,5 +16,47 @@ namespace forces
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBox1.Text);
+                double ans = sin(a);
+                label1.Text = ans.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Enter as Number");
+            }
+        }
+
+        double sin(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Sin(rad);
+        }
+        double inversesin(double a)
+        {
+            return Math.Asin(a) * 180.0 / Math.PI
+        }
+        double cos(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Cos(rad);
+        }
+        double inversecos(double a)
+        {
+            return Math.Acos(a) * 180.0 / Math.PI
+        }
+        double tan(double a)
+        {
+            double rad = a / 180.0 * Math.PI;
+            return Math.Tan(rad);
+        }
+        double inversetan(double a)
+        {
+            return Math.Atan(a) * 180.0 / Math.PI
+        }
     }
 }
